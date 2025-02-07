c3adc6c52a338fc7afdba51fb2c8f8af3105314e
Nicole Shek <nicoleshek@unknown.home>
2025-02-07 22:17:55 +0000

    update api as updated on frontend mentor, but cors error persists

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
   constructor(private http: HttpClient) {}
 
-  rootURL = 'https://api.shrtco.de/v2/';
+  rootURL = 'https://cleanuri.com/api/v1/shorten';
 
   getShortLink(longLink: string) {
-    return this.http.get(this.rootURL + 'shorten?url=' + longLink);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+    const body = `url=${encodeURIComponent(longLink)}`;
+
+    return this.http.post(this.rootURL, body, {headers});
   }
 }
